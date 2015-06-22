3e5f1835af9cbe66cdf13595c9c573cee7e15f56
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-06-22 20:06:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MastermindGame::Application.config.secret_key_base = '903f4ce2a3925c689e0e13ba361173b308f5279d7b9a9c6d083af0aa50b762383b038f0111e3f83293ce4cda2ac6dc53e377faaf8fc7c70c7716cb181890e102'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MastermindGame::Application.config.session_store :cookie_store, key: '_mastermind_game_session'
